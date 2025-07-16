786d74ea50196224755bd18c285dda24876330dd
johnvesslyalti <altijohnvessly@gmail.com>
2025-07-16 15:12:55 +0530

    feat: add Prisma adapter and update authentication configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,7 +1,10 @@
-import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
+import { db } from "@/lib/db";
+import NextAuth from "next-auth";
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+    adapter: PrismaAdapter(db),
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID!,

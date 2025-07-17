d8737ce556a31ccb7f4b7cd2e6e7461f01725efa
johnvesslyalti <altijohnvessly@gmail.com>
2025-07-17 10:09:09 +0530

    refactor: remove gradient background from Page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import HowItWorks from "@/components/HowItWorks";
 
 export default function Page() {
     return (
-        <div className="bg-gradient-to-br from-[#0f0c29] via-[#302b63] to-[#24243e]">
+        <div>
             <Hero />
             <Features />
             <HowItWorks />

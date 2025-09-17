f7ae633d2769082f9d8ab0f469cc47420b4c3f5c
johnvesslyalti <altijohnvessly@gmail.com>
2025-09-17 16:45:51 +0530

    Refactor code structure for improved readability and maintainability

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -15,8 +15,11 @@ const avatarOptions = [
   "/avatar1.png",
   "/avatar2.png",
   "/avatar3.png",
-  "/avatar1.png",
-  "/avatar1.png",
+  "/avatar4.png",
+  "/avatar5.png",
+  "/avatar6.png",
+  "/avatar7.png",
+  "/avatar8.png"
 ];
 
 export default function ProfilePage() {

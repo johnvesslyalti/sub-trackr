795229c0a616888c9c7c294b57cf6756611ffc74
johnvesslyalti <altijohnvessly@gmail.com>
2025-12-11 19:19:12 +0530

    feat: change the handle of X

--- src/components/landing-page/Hero.tsx
+++ src/components/landing-page/Hero.tsx
@@ -120,7 +120,7 @@ export default function Hero() {
             </a>
 
             <a
-              href="https://x.com/yourhandle"
+              href="https://x.com/johnveslyalti"
               target="_blank"
               className="hover:text-gray-400 transition"
             >
